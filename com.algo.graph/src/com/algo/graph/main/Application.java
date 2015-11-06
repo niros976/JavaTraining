302e27ee3422a2ddfb3914153ef45bd8a0b37ad6
Niros <niros976@yahoo.com>
2015-11-06 23:01:18 +0200

    initial commit

--- com.algo.graph/src/com/algo/graph/main/Application.java
+++ com.algo.graph/src/com/algo/graph/main/Application.java
@@ -1,10 +1,10 @@
-package com.learn.algo.main;
+package com.algo.graph.main;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+
 	}
 
 }
